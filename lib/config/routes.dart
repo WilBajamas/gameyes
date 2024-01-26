53dce4d336018cb52ab7ccc24207e6c61d5f7f9d
Wilbert Tan <wilbert@snappymob.com>
2024-01-26 18:01:51 +0800

    Add analysis options rules and base home screen

---
+++ lib/config/routes.dart
@@ -0,0 +1,6 @@
+import 'package:gaming_library_assessment_flutter/core/res/const.dart';
+import 'package:gaming_library_assessment_flutter/features/home/presentation/screen/home_screen.dart';
+
+final routes = {
+  RouteConstants.root: (context) => const HomeScreen(),
+};
