a641357904b3baa5753bce86c3a6603c211e8243
Wilbert Tan <wilbert@snappymob.com>
2024-01-28 23:47:24 +0800

    Remove unecessary imports

--- lib/features/featured/presentation/screen/featured_screen.dart
+++ lib/features/featured/presentation/screen/featured_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gaming_library_assessment_flutter/core/res/const.dart';
