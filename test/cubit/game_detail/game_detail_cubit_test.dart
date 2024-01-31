9c0b999fc8425be4cfb5363bbf6053411935517a
Wilbert Tan <wilbert@snappymob.com>
2024-01-31 23:35:18 +0800

    Create test for game detail cubit

---
+++ test/cubit/game_detail/game_detail_cubit_test.dart
@@ -0,0 +1,84 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gaming_library_assessment_flutter/features/game_detail/domain/repository/game_detail_repository.dart';
+import 'package:gaming_library_assessment_flutter/features/game_detail/presentation/cubit/game_detail_cubit.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:bloc_test/bloc_test.dart';
+
+import '../../mocks/error_mock.dart';
+import '../../mocks/game_detail_response_mock.dart';
+import 'game_detail_cubit_test.mocks.dart';
+
+@GenerateMocks([GameDetailRepository])
+void main() {
+  late GameDetailRepository gameDetailRepository;
+  late GameDetailCubit gameDetailCubit;
+
+  setUp(() {
+    gameDetailRepository = MockGameDetailRepository();
+
+    GetIt.I.registerSingleton(gameDetailRepository);
+
+    gameDetailCubit = GameDetailCubit();
+  });
+
+  tearDown(() {
+    GetIt.instance.reset();
+    reset(gameDetailRepository);
+  });
+
+  test('initial state is empty GameDetailState', () {
+    expect(
+      gameDetailCubit.state,
+      const GameDetailState(),
+    );
+  });
+
+  blocTest(
+    // ignore: lines_longer_than_80_chars
+    'emits GameDetailState with loading then GameDetailState with success when fetchGameDetail is called',
+    setUp: () {
+      when(
+        gameDetailRepository.fetchGameDetail(
+          id: 1,
+        ),
+      ).thenAnswer((_) async => Right(mockGameDetailResponse));
+    },
+    build: () => gameDetailCubit,
+    act: (cubit) async => gameDetailCubit.fetchGameDetail(
+      id: 1,
+    ),
+    expect: () => [
+      const GameDetailState(status: GameDetailStatus.loading),
+      GameDetailState(
+        status: GameDetailStatus.success,
+        response: mockGameDetailResponse,
+      ),
+    ],
+  );
+
+  blocTest(
+    // ignore: lines_longer_than_80_chars
+    'emits GameDetailState with loading then GameDetailState with failure when fetchGameDetail is called',
+    setUp: () {
+      when(
+        gameDetailRepository.fetchGameDetail(
+          id: 1,
+        ),
+      ).thenAnswer((_) async => Left(mockResponseError));
+    },
+    build: () => gameDetailCubit,
+    act: (cubit) async => gameDetailCubit.fetchGameDetail(
+      id: 1,
+    ),
+    expect: () => [
+      const GameDetailState(status: GameDetailStatus.loading),
+      GameDetailState(
+        status: GameDetailStatus.failed,
+        error: mockResponseError,
+      ),
+    ],
+  );
+}
---
+++ test/mocks/platform_mock.dart
@@ -0,0 +1,3 @@
+import 'package:gaming_library_assessment_flutter/features/game_detail/data/models/platform.dart';
+
+Platform get mockPlatform => const Platform(1, 'test platform name');
