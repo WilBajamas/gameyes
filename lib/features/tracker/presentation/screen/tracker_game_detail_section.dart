67250d2d051ccb55004dce43c1122da3431b6fb2
Wilbert Tan <wilbert@snappymob.com>
2024-04-30 23:21:05 +0800

    Remove task pie chart

--- lib/features/tracker/presentation/screen/tracker_game_detail_section.dart
+++ lib/features/tracker/presentation/screen/tracker_game_detail_section.dart
@@ -1,8 +1,6 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:gaming_library_assessment_flutter/core/enums/game_platform.dart';
 import 'package:gaming_library_assessment_flutter/core/utils/extensions.dart';
-import 'package:gaming_library_assessment_flutter/widgets/legend_indicator.dart';
 import 'package:gaming_library_assessment_flutter/widgets/task_item.dart';
 
 class TrackerGameDetailSection extends StatelessWidget {
@@ -14,10 +12,6 @@ class TrackerGameDetailSection extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 14),
       shrinkWrap: true,
       children: const [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 12),
-          child: _TaskCompletionProgress(),
-        ),
         SizedBox(height: 8),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 12),
@@ -30,90 +24,6 @@ class TrackerGameDetailSection extends StatelessWidget {
         ),
         SizedBox(height: 8),
       ],
-    );
-  }
-}
-
-class _TaskCompletionProgress extends StatelessWidget {
-  const _TaskCompletionProgress();
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: context.screenHeight * 0.3,
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                context.localisations.task_statuses,
-                style: context.themeData.textTheme.titleMedium,
-              ),
-              const SizedBox(height: 8),
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: PieChart(
-                        PieChartData(
-                          centerSpaceRadius: double.infinity,
-                          sections: [
-                            PieChartSectionData(value: 25, color: Colors.blue),
-                            PieChartSectionData(
-                              value: 25,
-                              color: Colors.yellow,
-                            ),
-                            PieChartSectionData(
-                              value: 25,
-                              color: Colors.orange,
-                            ),
-                            PieChartSectionData(value: 25, color: Colors.green),
-                            PieChartSectionData(
-                              value: 25,
-                              color: Colors.blueGrey,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        LegendIndicator(
-                          color: Colors.blue,
-                          text: context.localisations.done,
-                        ),
-                        LegendIndicator(
-                          color: Colors.yellow,
-                          text: context.localisations.inProgress,
-                        ),
-                        LegendIndicator(
-                          color: Colors.orange,
-                          text: context.localisations.missed,
-                        ),
-                        LegendIndicator(
-                          color: Colors.green,
-                          text: context.localisations.rageQuit,
-                        ),
-                        LegendIndicator(
-                          color: Colors.blueGrey,
-                          text: context.localisations.improve,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
